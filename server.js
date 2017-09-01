b50e19929ae814e62a35eb96f576530a80f556dd
andy <andy@ashall.co>
2017-09-01 16:00:38 +0100

    added build for prod

--- server.js
+++ server.js
@@ -6,38 +6,42 @@ const compression = require('compression')
 const isDeveloping = process.env.NODE_ENV !== 'production'
 const port = isDeveloping ? 3000 : process.env.PORT
 
-var webpack = require('webpack')
-var webpackDevServer = require('webpack-dev-server')
-var webpackConfig = require('./webpack.dev.conf')
+if (isDeveloping) {
 
-var compiler = webpack(webpackConfig)
+  const webpack = require('webpack')
+  const webpackDevServer = require('webpack-dev-server')
+  const webpackConfig = require('./webpack.dev.conf')
 
-var server = new webpackDevServer(compiler, {
-  clientLogLevel: "none",
-  contentBase: __dirname,
-  publicPath: "http://localhost:" + port,
-  historyApiFallback: true,
-  noInfo: true,
-  port: port,
-  watchContentBase: true
-})
+  const compiler = webpack(webpackConfig)
 
-server.listen(port, "127.0.0.1", function () {
-  console.log(`Project is running at http://localhost:${port}`)
-})
+  const server = new webpackDevServer(compiler, {
+    clientLogLevel: "none",
+    contentBase: __dirname,
+    publicPath: "http://localhost:" + port,
+    historyApiFallback: true,
+    noInfo: true,
+    port: port,
+    watchContentBase: true
+  })
 
-// const app = express()
+  server.listen(port, "127.0.0.1", function () {
+    console.log(`Project is running at http://localhost:${port}`)
+  })
 
-// app.use(compression())
+} else {
 
-// app.use(express.static(__dirname))
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, './index.html'))
-// })
+  const app = express()
+  app.use(compression())
+  app.use(express.static(__dirname + '/dist'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'))
+  }) 
 
-// app.listen(port, '0.0.0.0', (err) => {
-//   if (err) {
-//     console.log(err)
-//   }
-//   console.info(`Listening on port ${port}`)
-// })
+  app.listen(port, '0.0.0.0', (err) => {
+    if (err) {
+      console.log(err)
+    }
+    console.info(`Listening on port ${port}`)
+  })
+  
+}
